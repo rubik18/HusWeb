a12d69d128db042e74221b58421b4abd052785db
Dung <dunghy2000@gmail.com>
2020-08-06 16:27:45 +0700

    Dung

--- admin/SourceFile/add.php
+++ admin/SourceFile/add.php
@@ -10,18 +10,28 @@ $conn->set_charset("utf8");
   $id_category =$_POST['type'];
   if(!empty($_POST['tag'])){
   	$tag = $_POST['tag'];
-  	$len = sizeof($tag); 
-  }
-  if(!empty($_POST)) {
-		var_dump($_POST);
-		$sql = "INSERT INTO `new` ( `id_category`, `title`, `description`, `content`, `start_date`, `end_date`) VALUES ( '$id_category', '$title', '$description', '$content', '', '')";
-		mysqli_query($conn,$sql) or die("Thêm dữ liệu thất bại !");
-		$news_id = mysqli_insert_id($conn);
-		for ($i=0; $i < $len; $i++) { 
-			$sql1 = "INSERT INTO `news_tag`(`news_id`,`tags_id`) VALUES ('$news_id','$tag[$i]')";
-			mysqli_query($conn,$sql1) or die("Thêm tag thất bại!");
+	  if(!empty($_POST)) {
+			$sql = "INSERT INTO `new` ( `id_category`, `title`, `description`, `content`, `start_date`, `end_date`) VALUES ( '$id_category', '$title', '$description', '$content', '', '')";
+			mysqli_query($conn,$sql) or die("Thêm dữ liệu thất bại !");
+			$news_id = mysqli_insert_id($conn);
+			$v = "";
+			foreach ($tag as $k=>$value) {
+				if($k == 0){
+				$v .= "(".$news_id.", ".$value.")";
+				} else {
+					$v .= ", (".$news_id.", ".$value.")";
+				}
+			}
+			$sql1 = "INSERT INTO `news_tag`(`news_id`,`tags_id`) VALUES $v";
+			// var_dump($sql1);die();
+				mysqli_query($conn,$sql1) or die("Thêm tag thất bại!");
+			header("Location:table-master.php");
 		}
-		header("Location:table-master.php");
 	}
+  else{
+  	$sql = "INSERT INTO `new` ( `id_category`, `title`, `description`, `content`, `start_date`, `end_date`) VALUES ( '$id_category', '$title', '$description', '$content', '', '')";
+	mysqli_query($conn,$sql) or die("Thêm dữ liệu thất bại !");
+	header("Location:table-master.php");
+  }
  ?>
 
