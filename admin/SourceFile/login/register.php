dc2a85128254823a1efc836fcd27ec92bd822ebb
Dung <dunghy2000@gmail.com>
2020-08-10 11:08:59 +0700

    Dung

--- admin/SourceFile/login/register.php
+++ admin/SourceFile/login/register.php
@@ -28,9 +28,9 @@
     <div class="card-body register-card-body">
       <p class="login-box-msg">Register a new membership</p>
 
-      <form action="../index.html" method="post">
+      <form action="register_submit.php" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
+          <input type="text" class="form-control" name = "full_name" placeholder="Full name">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -38,7 +38,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" name = "user_name"placeholder="Username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -46,7 +46,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" name = "password"placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -54,7 +54,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" class="form-control" name = "repassword"placeholder="Retype password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -63,12 +63,6 @@
         </div>
         <div class="row">
           <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-              <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
-              </label>
-            </div>
           </div>
           <!-- /.col -->
           <div class="col-4">
---
+++ admin/SourceFile/login/register_submit.php
@@ -0,0 +1,18 @@
+<?php 
+//connect
+require '../db/connect.php';
+// kiểm tra điền đầy đủ thông tin
+$username = $_POST["user_name"];
+$fullname = $_POST["full_name"];
+$password = $_POST["password"];
+$repassword = $_POST["repassword"];
+
+if ( isset($_POST['submit']) && $username !='' && $fullname !='' && $password !='' && $repassword !='' ) {
+  if ( $password != $repassword) {
+  	header("Location:register.php");
+  }
+}
+else{
+  header("Location:register.php");
+}
+ ?>
