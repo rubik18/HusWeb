a57d4e570d47f333b953011f2147694ca4a1888b
ninh_jvb <nguyenvanninh.jvb@gmail.com>
2020-08-10 09:12:09 +0700

    layout-n

--- frontend/hoc-sinh-sinh-vien/hoc-bong-giai-thuong.php
+++ frontend/hoc-sinh-sinh-vien/hoc-bong-giai-thuong.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html class="no-js" lang="vi-VN" style="scroll-behavior: smooth;">
-<head id="Head">
-    <title>VNU - Hanoi University of Science</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="../../css/themify-icons-n.css">
-    <link rel="stylesheet" type="text/css" href="../../css/font-awesome-n.css">
-    <link rel="stylesheet" type="text/css" href="../../css/style-n.css">
-    <link rel="stylesheet" type="text/css" href="../../css/swipebox-n.css">
-    <link rel="stylesheet" type="text/css" href="../../css/owl-n.css">
-    <link rel="stylesheet" type="text/css" href="../../css/menu.css">
-    <link rel="icon" href="http://hus.vnu.edu.vn/favicon.ico" type="image/ico" sizes="16x16">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-</head>
-<body id="Body">
+<title>Học bổng giải thưởng</title>
     <!---start header----->
     <?php include "../header.html" ?>
     <!----end header------->
@@ -127,11 +105,11 @@
                                         <li class="active"><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/hoc-bong-giai-thuong.php">Học bổng, giải thưởng</a></li>
                                         <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien.php" class=""><span class="ink animate-ink" style="height: 359px; width: 359px; top: -161.5px; left: 149.344px;"></span>Hỗ trợ sinh viên<span class="submenu-indicator">+</span></a>
                                             <ul class="submenu" id="5496">
-                                                <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/doan-hoi.php">Đoàn - Hội</a></li>
-                                                <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/doi-song.php">Đời sống</a></li>
-                                                <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/cac-cau-lac-bo.php">Các câu lạc bộ</a></li>
-                                                <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/vay-von.php">Vay vốn</a></li>
-                                                <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/tu-van-ho-tro-viec-lam.php">Tư vấn, hỗ trợ việc làm</a></li>
+                                                <li><a class=" dropdown-item" href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien/doan-hoi.php">Đoàn - Hội</a></li>
+                                                <li><a class=" dropdown-item" href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien/doi-song.php">Đời sống</a></li>
+                                                <li><a class=" dropdown-item" href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien/cac-cau-lac-bo.php">Các câu lạc bộ</a></li>
+                                                <li><a class=" dropdown-item" href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien/vay-von.php">Vay vốn</a></li>
+                                                <li><a class=" dropdown-item" href="/php-intership-team/frontend/hoc-sinh-sinh-vien/ho-tro-sinh-vien/tu-van-ho-tro-viec-lam.php">Tư vấn hỗ trợ việc làm</a></li>
                                             </ul>
                                         </li>
                                         <li class=""><a href="/php-intership-team/frontend/hoc-sinh-sinh-vien/nghien-cuu-khoa-hoc.php">Nghiên cứu khoa học</a></li>
@@ -163,5 +141,3 @@
     <!---start footer----->
     <?php include "../footer.html" ?>
     <!----end footer------->
-</body>
-</html>
