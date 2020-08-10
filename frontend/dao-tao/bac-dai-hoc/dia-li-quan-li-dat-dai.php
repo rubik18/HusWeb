f79c6fe768772f3f0ccbab4dde29617ff1ecdf22
Hoang Duc <hoangductran140320@gmail.com>
2020-08-10 18:06:46 +0700

    duc1008

---
+++ frontend/dao-tao/bac-dai-hoc/dia-li-quan-li-dat-dai.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Địa lý, Quản lý đất đai và GISc</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">     
+    <link rel="stylesheet" type="text/css" href="/php-intership-team/css/bacdaihoc.css">
+
+    <link rel="stylesheet" type="text/css" href="/php-intership-team/css/chen-slide.css">
+
+</head>
+<body id="Body">
+
+    <?php
+    include "../../header.html"
+    ?>
+    <!-- hết header -->
+
+    <!-- content -->
+	<div class="main-content-wrapper">
+        <div class="container">
+            <div class="breakcrum mb-30">
+                <div class="row">
+                    <div class="col-12">
+                        <ol class="breadcrumb">
+                            
+                            <li><a href="\php-intership-team\frontend\dao-tao\gioi-thieu\daotao.php" class="item1">Đào tạo</a></li>&nbsp;
+
+                            <img alt="*" src="http://hus.vnu.edu.vn/images/breadcrumb.gif">&nbsp;
+
+                            <li><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\bac-dai-hoc.php" class="item1">Bậc Đại học</a></li>&nbsp;
+
+                            <img alt="*" src="http://hus.vnu.edu.vn/images/breadcrumb.gif">&nbsp;
+
+                            <li><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\dia-li-quan-li-dat-dai.php" class="item1">Địa lý, Quản lý đất đai và GISc</a></li>
+
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="world-latest-articles">
+                <div class="row">
+                    <div class="col-12 col-lg-12">
+                        <div class="single-blog-content mb-50">
+                            <div id="dnn_ContentPane">
+
+                                <div class="DnnModule DnnModule-Navigation-ShowPageSub DnnModule-12659"><a name="12659"></a>
+
+                                    <style>
+                                #jquery-accordion-menu .jquery-accordion-menu-header,
+                                #jquery-accordion-menu .jquery-accordion-menu-footer
+                                 {display:none}
+                                #jquery-accordion-menu {
+                                    background: none !important;
+                                    color: #fff;
+                                    transition: all 0.3s;
+                                    border:none !important;margin: 0 auto;
+                                text-align: center;
+                                }
+
+                                    #jquery-accordion-menu ul.components {
+                                        padding: 0px 0;
+                                        text-align: center !important;
+                                        border: 0 none;
+                                        margin-bottom: 30px;
+                                        width: 100%;
+                                        list-style: outside none none !important;
+                                    }
+
+                                        #jquery-accordion-menu ul.components:before {
+                                            display: table;
+                                            content: " ";
+                                        }
+
+                                        #jquery-accordion-menu ul.components:after {
+                                            clear: both;
+                                        }
+
+                                        #jquery-accordion-menu ul.components p {
+                                            display: none;
+                                        }
+
+                                    #jquery-accordion-menu ul li {
+                                        padding: 0px;
+                                        float: none;
+                                        display: inline-block !important;
+                                        margin: 0 auto;
+                                        display: inline;width: auto;
+                                        zoom: 1;    margin: 0px 5px;border:0px;
+                                    }
+
+                                        #jquery-accordion-menu ul li a {
+                                            padding: 10px 20px !important;
+                                            display: block;
+                                            border: 1px solid #e6e6e6;
+                                            border-radius: 30px;
+                                            color: #3b3b3b;
+                                            margin-right: 8px;
+                                            line-height: 1.42857143;
+                                            position: relative;
+                                            font-size: 14px !important;
+                                            margin-bottom: 10px; font-weight: 400;
+                                        }
+                                        #jquery-accordion-menu ul li a:hover {
+                                             font-weight: 400;
+                                        }
+                                        #jquery-accordion-menu ul li.active > a, a[aria-expanded="true"] {
+                                            color: #fff !important;
+                                            background: #0067a2 !important;
+                                            border:none !important;    font-weight: 400;
+
+                                        }
+                                    </style>
+
+
+                                    <div id="dnn_ctr12659_ContentPane"><!-- Start_Module_12659 -->
+                                <div id="dnn_ctr12659_ModuleContent">
+    
+                                <link href="http://hus.vnu.edu.vn/DesktopModules/NV_Navgiation/Display/Khoastyle.css" rel="stylesheet">
+
+                                    <div class="content">
+                                        <div id="jquery-accordion-menu" class="jquery-accordion-menu">
+                                            <div class="jquery-accordion-menu-header">Bậc Đại học </div>
+
+                                            <ul>
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\dia-chat.php">Địa chất</a></li>
+
+                                                <li class="active"><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\dia-li-quan-li-dat-dai.php">Địa lý, Quản lý đất đai và GISc</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\khi-tuong.php">Khí tượng, Khoa học và Công nghệ biển, Tài nguyên và Môi trường nước</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\hoa-hoc.php">Hóa học và Công nghệ</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\tai-nguyen-moi-truong.php" style="color: black;">Quản lý Tài nguyên Môi trường, Địa chất Ứng dụng</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\sinh-hoc-va-cong-nghe.php" style="color: black;">Sinh học và Công nghệ</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\toan-ung-dung.php" style="color: black;">Toán, Toán ứng dụng và Tin học</a></li>
+
+                                                <li class=""><a href="\php-intership-team\frontend\dao-tao\bac-dai-hoc\vat-li.php" style="color: black;">Vật lí và Công nghệ</a></li>
+            
+                                            </ul>
+
+                                            <div class="jquery-accordion-menu-footer"></div>
+                                        </div>
+                                    </div>
+
+
+                                    
+
+                                </div><!-- End_Module_12659 -->
+                                    </div>
+
+
+                                </div>
+
+                                <div class="DnnModule DnnModule-Tintc-HUSDaotao DnnModule-12647"><a name="12647"></a>
+                                    <div id="dnn_ctr12647_ContentPane"><!-- Start_Module_12647 -->
+                                        <div id="dnn_ctr12647_ModuleContent">
+    
+                                            <link rel="stylesheet" href="/php-intership-team/css/chen-slide.css">
+
+                                                <div id="verticalTab" class="resp-vtabs" style="display: block; width: 100%; margin: 0px;">
+                                                    <ul class="resp-tabs-list">
+            
+                                                        <li class="resp-tab-item resp-tab-active" aria-controls="tab_item-0" role="tab">Quản lí phát triển đô thị và bất động sản</li>
+                                                    
+                                                        <li class="resp-tab-item" aria-controls="tab_item-1" role="tab">Địa lý tự nhiên</li>
+                                                    
+                                                        <li class="resp-tab-item" aria-controls="tab_item-2" role="tab">Địa lý tự nhiên chất lượng cao</li>
+                                                    
+                                                        <li class="resp-tab-item" aria-controls="tab_item-3" role="tab">Quản lý đất đai</li>
+                                                    
+                                                    </ul>
+
+                                                    <div class="resp-tabs-container">
+            
+                    
+                                                        <h2 class="resp-accordion resp-tab-active" role="tab" aria-controls="tab_item-0"><span class="resp-arrow"></span>Quản lí phát triển đô thị và bất động sản</h2>
+
+                                                        <div class="resp-tab-content resp-tab-content-active" style="display:block" aria-labelledby="tab_item-0">
+
+                                                            <div class="tieude"><a href="#" class="item1">Quản lí phát triển đô thị và bất động sản</a>
+                                                            </div>
+                                                            <p style="text-align: center;"><iframe height="700px" src="http://hus.vnu.edu.vn/DATA/IMAGES/2020/06/nganhqlptdt_bds.pdf" width="100%"></iframe></p>
+
+                                                            <p>&nbsp;</p>
+
+                                                        </div>
+                
+                    
+                                                        <h2 class="resp-accordion" role="tab" aria-controls="tab_item-1"><span class="resp-arrow"></span>Địa lý tự nhiên</h2>
+
+                                                        <div class="resp-tab-content" aria-labelledby="tab_item-1">
+                                                            <div class="tieude"><a href="#" class="item1">Địa lý tự nhiên</a>
+                                                            </div>
+
+                                                            <p style="text-align: center;"><iframe src="http://hus.vnu.edu.vn/DATA/IMAGES/2019/07/1. Chuong trinh dao tao chuan nganh Dia ly tu nhien.pdf" width="100%" height="700px"></iframe></p><br>
+                                                        </div>
+                
+                    
+                                                        <h2 class="resp-accordion" role="tab" aria-controls="tab_item-2"><span class="resp-arrow"></span>Địa lý tự nhiên chất lượng cao</h2>
+
+                                                        <div class="resp-tab-content" aria-labelledby="tab_item-2">
+                                                            <div class="tieude"><a href="#" class="item1">Địa lý tự nhiên chất lượng cao</a>
+                                                            </div>
+                                                            <p style="text-align: center;"><iframe src="http://hus.vnu.edu.vn/DATA/IMAGES/2019/07/3. Chuong trinh dao tao chat luong cao nganh Dia ly tu nhien.pdf" width="100%" height="700px"></iframe></p><br>
+                                                        </div>
+                
+                    
+                                                        <h2 class="resp-accordion" role="tab" aria-controls="tab_item-3"><span class="resp-arrow"></span>Quản lý đất đai</h2>
+
+                                                        <div class="resp-tab-content" aria-labelledby="tab_item-3">
+                                                            <div class="tieude"><a href="#" class="item1">Quản lý đất đai</a>
+                                                            </div>
+                                                            <p style="text-align: center;"><iframe src="http://hus.vnu.edu.vn/DATA/IMAGES/2019/07/2. Chuong trinh dao tao chuan nganh Quan ly dat dai.pdf" width="100%" height="700px"></iframe></p><br>
+                                                        </div>
+                
+                                                    </div>
+                                                </div>
+
+
+                                                <script>
+                                                    $(document).ready(function () {
+                                                        $('#verticalTab').easyResponsiveTabs({
+                                                            type: 'vertical',
+                                                            width: 'auto',
+                                                            fit: true
+                                                        });
+                                                    });
+                                                </script>
+
+                                                <script src="/php-intership-team/js/chenslide.js"></script>
+
+                                        </div><!-- End_Module_12647 -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Blog Post -->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- footer -->
+ 
+    <?php
+        include "../../footer.html"
+    ?>
+
+</body>
+</html>
