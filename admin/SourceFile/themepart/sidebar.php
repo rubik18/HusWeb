af2ce3988c0a9ece48c1002d087434f87b31d756
Dung <dunghy2000@gmail.com>
2020-08-10 17:18:50 +0700

    Dung

--- admin/SourceFile/themepart/sidebar.php
+++ admin/SourceFile/themepart/sidebar.php
@@ -13,7 +13,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/DAPM/php-intership-team/admin/SourceFile/myimg/ava-TNR.png" class="img-circle elevation-2" alt="User Image">
+          <img src="/php-intership-team/admin/SourceFile/myimg/ava-TNR.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Admin</a>
