2fcf46f9d81892008516204f9cd282c0e574858c
ninh_jvb <nguyenvanninh.jvb@gmail.com>
2020-08-13 15:37:35 +0700

    ninh-1308

---
+++ frontend/main/thu-vien-media.php
@@ -0,0 +1,11 @@
+<title>Thư viện Ảnh</title>
+
+	<!---start header----->
+    <?php include "../header.html" ?>
+    <!----end header------->
+
+    
+    
+    <!---start footer----->
+    <?php include "../footer.html" ?>
+    <!----end footer------->
