fba63322eb97f2012481deca931d2de402b200dc
Dung <dunghy2000@gmail.com>
2020-08-20 13:20:01 +0700

    mota

--- admin/SourceFile/project_research/update-project.php
+++ admin/SourceFile/project_research/update-project.php
@@ -11,7 +11,10 @@
   $acceptance_date = $_POST['acceptance_date'];
   $result = $_POST['result'];
   $id_type = $_POST['type'];
-		 $s = "UPDATE `topic_project`,`type_topic_project` SET `id_type`='$id_type',`id_project`='$id_project',`name`='$name',`lead_researcher`='$leader',`workplace` = '$workplace',`approval_date` = '$approval_date',`acceptance_date` = '$acceptance_date',`result` = '$result' WHERE topic_project.id= {$id} AND topic_project.id_type = type_topic_project.id";
-     mysqli_query($conn,$s) or die("Sửa dữ liệu thất bại !");
-		 header("Location:./table-project.php");
+		 $sql = "UPDATE `topic_project`,`type_topic_project` SET `id_type`='$id_type',`id_project`='$id_project',`name`='$name',`lead_researcher`='$leader',`workplace` = '$workplace',`approval_date` = '$approval_date',`acceptance_date` = '$acceptance_date',`result` = '$result' WHERE topic_project.id= {$id} AND topic_project.id_type = type_topic_project.id";
+     mysqli_query($conn,$sql);
+     if (!mysqli_query($conn,$sql)) {
+  die('error:'. mysqli_error($conn));
+}
+   header("Location:./table-project.php");
  ?>
