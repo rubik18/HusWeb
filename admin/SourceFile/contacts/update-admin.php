2575c88698137eecc10d70cdcb5ee28efa087276
Hoang Duc <hoangductran140320@gmail.com>
2020-08-25 14:42:32 +0700

    edit2508

--- admin/SourceFile/contacts/update-admin.php
+++ admin/SourceFile/contacts/update-admin.php
@@ -8,9 +8,7 @@
   // var_dump($_POST); die();
   $user_name = $_POST['user_name'];
   $full_name = $_POST['full_name'];
-  $password = md5($_POST['password']);
-  $repassword = md5($_POST['repassword']);
-  $lever = $_POST["type"];
+  $level = $_POST['level'];
   $file_name=$_FILES['fileupload']["name"];
   //lấy đường dẫn tạm lưu nội dung file:
   $file_tmp =$_FILES['fileupload']['tmp_name'];
@@ -22,14 +20,20 @@ move_uploaded_file($file_tmp,$path);
   if (isset($_FILES['fileupload'])&&$_FILES['fileupload']["name"]!=null) {
     $avatar = $link;
   }
-  else{
+  elseif(isset($_FILES['fileupload'])&&$_FILES['fileupload']["name"] = null ){
     $avatar = $_POST['URL'];
+  }
+  else{
+    $avatar = "/php-intership-team/admin/SourceFile/login/upload/admin.jpg";  
   }
 
     if(!empty($_POST)) {
       // var_dump($_POST);
-  		$sql = "UPDATE `user` SET `user_name`='$user_name', `full_name`='$full_name',`avatar`='$avatar',`password`='$password' WHERE user.id= {$id}";
-  		mysqli_query($conn,$sql) or die("Sửa dữ liệu thất bại !");
+  		$sql = "UPDATE `user` SET `user_name`='$user_name', `full_name`='$full_name',`avatar`='$avatar', `level` = '$level' WHERE user.id= {$id}";
+  		$result = mysqli_query($conn,$sql);
+      if (!$result) {
+        die('error'.mysqli_error($conn));
+      }
   		header("Location: \php-intership-team\admin\SourceFile\contacts\contacts-table.php");
   	}
  ?>
